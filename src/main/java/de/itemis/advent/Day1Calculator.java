81913c675d897b7bf7867e4272239c39ee22bf04
DJetzen <djetzen@users.noreply.github.com>
2021-09-10 09:46:27 +0200

    add solution for part 2 of day one

--- src/main/java/de/itemis/advent/Day1Calculator.java
+++ src/main/java/de/itemis/advent/Day1Calculator.java
@@ -3,22 +3,21 @@ package de.itemis.advent;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.*;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Day1Calculator {
-    public boolean isSum2020(int firstNumber, int secondNumber) {
-        return (firstNumber+secondNumber) == 2020;
+    private boolean isSum2020(List<Integer> numbers) {
+        return numbers.stream().mapToInt(Integer::intValue).sum() == 2020;
     }
 
-    public Set<Integer> linesFromFileWithSum2020(String filename) throws IOException {
+    private Set<Integer> linesFromFileWithTwoDigitsSummingUpTo2020(String filename) throws IOException {
         Set<Integer> result = new HashSet<>();
-        List<Integer> readNumbers = Files.readAllLines(Paths.get(filename)).stream().mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
-        for(int i: readNumbers) {
-            for(int j: readNumbers) {
-                if(i!=j && isSum2020(i,j)) {
+        List<Integer> readNumbers = readNumbersFromFile(filename);
+        for (int i : readNumbers) {
+            for (int j : readNumbers) {
+                if (i != j && isSum2020(List.of(i, j))) {
                     result.add(i);
                     result.add(j);
                 }
@@ -27,7 +26,33 @@ public class Day1Calculator {
         return result;
     }
 
-    public int calculateSolution(String filename) throws IOException {
-        return linesFromFileWithSum2020(filename).stream().reduce(1, (a,b) -> a*b);
+    private Set<Integer> linesFromFileWithThreeDigitsSummingUpTo2020(String filename) throws IOException {
+        Set<Integer> result = new HashSet<>();
+        List<Integer> readNumbers = readNumbersFromFile(filename);
+        for (int i : readNumbers) {
+            for (int j : readNumbers) {
+                for (int k : readNumbers) {
+                    if (i != j && j != k && i != k && isSum2020(List.of(i, j, k))) {
+                        result.add(i);
+                        result.add(j);
+                        result.add(k);
+                    }
+
+                }
+            }
+        }
+        return result;
+    }
+
+    private List<Integer> readNumbersFromFile(String filename) throws IOException {
+        return Files.readAllLines(Paths.get(filename)).stream().mapToInt(Integer::parseInt).boxed().toList();
+    }
+
+    public int calculateSolutionForPuzzleOne(String filename) throws IOException {
+        return linesFromFileWithTwoDigitsSummingUpTo2020(filename).stream().reduce(1, (a, b) -> a * b);
+    }
+
+    public int calculateSolutionForPuzzleTwo(String filename) throws IOException {
+        return linesFromFileWithThreeDigitsSummingUpTo2020(filename).stream().reduce(1, (a, b) -> a * b);
     }
 }
--- src/test/java/de/itemis/advent/Day1CalculatorTest.java
+++ src/test/java/de/itemis/advent/Day1CalculatorTest.java
@@ -3,62 +3,29 @@ package de.itemis.advent;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 class Day1CalculatorTest {
 
     @Test
-    void ifTwoNumbersSumUpTo2020ItIsRecognized() {
-        Day1Calculator day1Calculator = new Day1Calculator();
-        boolean result  = day1Calculator.isSum2020(2019,1);
-
-        assertTrue(result);
-
-    }
-
-    @Test
-    void ifTwoNumbersDoNotSumUpTo2020ItIsNotRecognized() {
-        Day1Calculator day1Calculator = new Day1Calculator();
-        boolean result  = day1Calculator.isSum2020(1,1);
-
-        assertFalse(result);
-    }
-
-    @Test
-    void ifThereAreNoNumbersThatSumUpTo2020AnEmptyListIsReturned() throws IOException {
-        String filename = "src/test/resources/day1/none_matching_numbers.txt";
+    void calculateSolutionForPartOne() throws IOException {
+        String filename = "src/main/resources/day1/input_day_1.txt";
         Day1Calculator day1Calculator = new Day1Calculator();
 
-        Set<Integer> numbersWithSum2020 = day1Calculator.linesFromFileWithSum2020(filename);
-
-        assertTrue(numbersWithSum2020.isEmpty());
-    }
-
-    @Test
-    void ifThereAreNumbersThatSumUpTo2020BothValuesAreReturned() throws IOException {
-        String filename = "src/test/resources/day1/matching_numbers.txt";
-        Day1Calculator day1Calculator = new Day1Calculator();
-
-        Set<Integer> numbersWithSum2020 = day1Calculator.linesFromFileWithSum2020(filename);
-
-        assertThat(numbersWithSum2020).hasSize(2);
-        assertThat(numbersWithSum2020).contains(2019);
-        assertThat(numbersWithSum2020).contains(1);
-    }
-
-    @Test
-    void calculateSolution() throws IOException {
-        String filename = "src/main/resources/day1/input_day_1_1.txt";
-        Day1Calculator day1Calculator = new Day1Calculator();
-
-        int solution = day1Calculator.calculateSolution(filename);
+        int solution = day1Calculator.calculateSolutionForPuzzleOne(filename);
 
         assertThat(solution).isEqualTo(605364);
+    }
+
+    @Test
+    void calculateSolutionForPartTwo() throws IOException {
+        String filename = "src/main/resources/day1/input_day_1.txt";
+        Day1Calculator day1Calculator = new Day1Calculator();
+
+        int solution = day1Calculator.calculateSolutionForPuzzleTwo(filename);
+
+        assertThat(solution).isEqualTo(128397680);
     }
 }
