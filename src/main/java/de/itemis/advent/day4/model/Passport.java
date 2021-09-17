9363de10c3e8e53136d6821b70eb1819155cc588
Dominik Jetzen <djetzen@users.noreply.github.com>
2021-09-17 08:30:56 +0200

    Add solution for day 4 (#1)

---
+++ src/main/java/de/itemis/advent/day4/model/Passport.java
@@ -0,0 +1,72 @@
+package de.itemis.advent.day4.model;
+
+import java.util.List;
+
+public record Passport(
+        String byr,
+        String iyr,
+        String eyr,
+        String hgt,
+        String hcl,
+        String ecl,
+        String pid,
+        String cid) {
+
+    public boolean isExtendedValid() {
+        return allElementsAreSet() && passesHaveValidEntries();
+    }
+
+    public boolean isSimpleValid() {
+        return allElementsAreSet();
+    }
+
+    private boolean allElementsAreSet() {
+        return byr != null && iyr != null && eyr != null && hgt != null && hcl != null && ecl != null && pid != null;
+    }
+
+    private boolean passesHaveValidEntries() {
+        return validateByr() && validateIyr() && validateEyr() && validateHgt() && validateHcl() && validateEcl() && validatePid();
+    }
+
+    private boolean validatePid() {
+        return this.pid.length() == 9;
+    }
+
+    private boolean validateEcl() {
+        var validColors = List.of("amb", "blu", "brn", "gry", "grn", "hzl", "oth");
+        return validColors.contains(this.ecl);
+    }
+
+    private boolean validateHcl() {
+        return this.hcl.matches("[#][0-9a-fA-F]{6}");
+    }
+
+    private boolean validateHgt() {
+        if (this.hgt.endsWith("cm")) {
+            var cmSize = Integer.parseInt(this.hgt.split("cm")[0]);
+            return cmSize >= 150 && cmSize <= 193;
+
+        } else if (this.hgt.endsWith("in")) {
+
+            var inSize = Integer.parseInt(this.hgt.split("in")[0]);
+            return inSize >= 59 && inSize <= 76;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean validateEyr() {
+        var e = Integer.parseInt(this.eyr);
+        return e >= 2020 && e <= 2030;
+    }
+
+    private boolean validateIyr() {
+        var i = Integer.parseInt(this.iyr);
+        return i >= 2010 && i <= 2020;
+    }
+
+    private boolean validateByr() {
+        var b = Integer.parseInt(this.byr);
+        return b >= 1920 && b <= 2002;
+    }
+}
